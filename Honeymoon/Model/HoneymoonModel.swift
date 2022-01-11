c80126af3dfef541845188c215e6b699c1a9e886
Yash Bhojgarhia <yashbhojgarhia@gmail.com>
2022-01-11 16:13:44 +0530

    added resources

---
+++ Honeymoon/Model/HoneymoonModel.swift
@@ -0,0 +1,14 @@
+//
+//  HoneymoonModel.swift
+//  Honeymoon
+//
+//  Created by Yash Bhojgarhia on 05/01/22.
+//
+
+import SwiftUI
+
+struct Destination {
+    var place: String
+    var country: String
+    var image: String
+}
