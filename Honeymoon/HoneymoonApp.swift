43d8d372ab1c2cffda31e204d108297e2172c2b8
Yash Bhojgarhia <yashbhojgarhia@gmail.com>
2022-01-11 16:09:36 +0530

    Initial Commit

---
+++ Honeymoon/HoneymoonApp.swift
@@ -0,0 +1,17 @@
+//
+//  HoneymoonApp.swift
+//  Honeymoon
+//
+//  Created by Yash Bhojgarhia on 11/01/22.
+//
+
+import SwiftUI
+
+@main
+struct HoneymoonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
